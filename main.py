8e5963f0bfe5d723d23470a3793be142f0d56769
EnisseNd <155125177+EnisseNd@users.noreply.github.com>
2024-01-01 21:51:47 +0000

    Added actual user choices and responses
    I deleted the placeholders and made actual questions the user could ask the chatbot and the chatbot's corresponding answers.

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ print(' How can I help you today?')
 
 def chat_options():
   print('Please choose from the following speech options:')
-  print('1. Placeholder Option 1 ') 
-  print('2. Placeholder Option 2')    
-  print('3. Placeholder Option 3')  
+  print('1. What day is it today? ') 
+  print('2. How are you?')    
+  print('3. What do you like to do?')  
   print('4. Exit the conversation') 
 chat_options()
 
 def user_selection():
   user_choice = int(input('Please enter your numerical choice: '))
   if user_choice == 1: 
-    print('\nPlaceholder Option 1')
+    print('\nDepends when you\'re asking me ;)')
   elif user_choice == 2:  
-    print('\nPlaceholder Option 2') 
+    print('\nFantastic, and you?') 
   elif user_choice == 3:  
-    print('\nPlaceholder Option 3')
+    print('\nChat with you of course!')
   elif user_choice == 4:  
-    print(f'\nGoodbye {name}! have an awesome day!')
+    print(f'\nGoodbye {name}! Talk to you later!')
   else:
     print("Sorry, That's not a valid choice. Please input one of the numbers!")
 user_selection()
