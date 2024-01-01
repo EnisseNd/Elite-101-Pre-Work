bc6b0e9e4bb892031bcd32397144f9a8fb2cc7d1
EnisseNd <155125177+EnisseNd@users.noreply.github.com>
2024-01-01 20:39:10 +0000

    Added Age and Asked How to Assist
    I added an age variable to store the user's age and simulated a convo in which I reminisce on being the users age again and end with asking how I can help the user.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 print('Welcome to my Elite 101 Chatbot!')
 name = input('Please enter your name: ')
-
-
+age = input(f' Hi {name}! How old are you? ')
+print('Nice to meet you ' + name + '!' + f' Oh how to be {age} again!', end='') 
+print(' How can I help you today?') 
 
