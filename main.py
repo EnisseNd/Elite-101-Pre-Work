6d9e3fbeecdbf6cdfe640f9c332e34ee1a9cfd1e
EnisseNd <155125177+EnisseNd@users.noreply.github.com>
2024-01-01 21:12:24 +0000

    Created and Called Function chat_options
    I created a function called chat_options, which shows the different options the user has when taking to the chatbot, including one to exit the conversation. I then call it right under.

--- main.py
+++ main.py
@@ -4,3 +4,14 @@ age = input(f' Hi {name}! How old are you? ')
 print('Nice to meet you ' + name + '!' + f' Oh how to be {age} again!', end='') 
 print(' How can I help you today?') 
 
+def chat_options():
+  print('Please choose from the following options:')
+  print('1. Placeholder Option 1 ') 
+  print('2. Placeholder Option 2')    
+  print('3. Placeholder Option 3')  
+  print('4. Exit the conversation') 
+chat_options()
+
+
+
+
